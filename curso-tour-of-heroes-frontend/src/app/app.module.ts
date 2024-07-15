2e004f511f1ec8356afd49dba29aba2d47238290
Altamiro Abrantes <altamiropina.psti@poupex.com.br>
2024-07-15 20:51:26 -0300

    Curso Angular v13: #12 - Angular Material

--- curso-tour-of-heroes-frontend/src/app/app.module.ts
+++ curso-tour-of-heroes-frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroesDetailComponent } from './heroes-detail/heroes-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HeroesDetailComponent } from './heroes-detail/heroes-detail.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
